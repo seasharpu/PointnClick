24589036233a0552cd94d2c8592e947c692bb635
olivialinnea <82369027+olivialinnea@users.noreply.github.com>
2021-12-14 15:16:09 +0100

    Skapade unika css-filer samt kopplade ihop dem.

--- index.php
+++ index.php
@@ -1,22 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/styles.css">
+    <link rel="stylesheet" href="assets/css/common.css">
+    <link rel="stylesheet" href="assets/css/start.css">
+    <link rel="stylesheet" href="assets/css/alien.css">
+    <link rel="stylesheet" href="assets/css/jungle.css">
+    <link rel="stylesheet" href="assets/css/pluto.css">
+    <link rel="stylesheet" href="assets/css/desert.css">
+    <link rel="stylesheet" href="assets/css/rave.css">
+    <link rel="stylesheet" href="assets/css/spaceship.css">
+    <link rel="stylesheet" href="assets/css/tutorial.css">
+    <link rel="stylesheet" href="assets/css/water.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
     <title>Saving Laika</title>
 </head>
-    <body>
-        <header>
-        </header>
-        <main>
-        </main>
-        <footer>
-            <h4 id="footer-tile">© 2021 - Click n'Point studios</h4>
-        </footer>
-    </body>
+
+<body>
+    <header>
+    </header>
+    <main>
+    </main>
+    <footer>
+        <h4 id="footer-tile">© 2021 - Click n'Point studios</h4>
+    </footer>
+</body>
+
 </html>
