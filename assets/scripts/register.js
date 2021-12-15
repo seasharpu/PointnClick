dd73aa19e96133ae994042daac53b79937fc14f6
mercieakerblom <mercie.akerblom@hotmail.com>
2021-12-15 11:48:05 +0100

    håller på med registera sidan

--- assets/scripts/register.js
+++ assets/scripts/register.js
@@ -3,9 +3,14 @@
 //REGISTER SIDA
 function MakerRegisterPage() {
     document.querySelector("main").style.backgroundImage = "url('assets/images/space_background.png')";
+ 
+    let GridWrapperDiv = document.createElement("div");
+    document.querySelector("main").append(GridWrapperDiv);
+    GridWrapperDiv.classList.add("GridWrapperDiv");
+
     let wrapperDiv = document.createElement("div");
     wrapperDiv.classList.add("wrapperDiv");
-    document.querySelector("main").append(wrapperDiv);
+    document.querySelector(".GridWrapperDiv").append(wrapperDiv);
 
     let signInDiv = document.createElement("div");
     signInDiv.classList.add("hej");
