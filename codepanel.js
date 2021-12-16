bcfc3b10b7ba666aa459a37c0e87c86163587fd6
AliceDuncalf <aliceduncalf@hotmail.se>
2021-12-16 11:41:34 +0100

    kodpanel

---
+++ codepanel.js
@@ -0,0 +1,97 @@
+"use strict";
+
+function codePanel() {
+    let codeWrapper = document.createElement("div"); //main wrapper för innehållet
+    document.getElementById("main").append(codeWrapper);
+    codeWrapper.classList.add("codeWrapper");
+    codeWrapper.innerHTML =`<h1 id="codePanelTitel">UNLOCK LAIKAS SHIP
+                                <p class= "codeMessage"></p>
+                            </h1>`;
+    
+    let codePanelIMGandNumbers = document.createElement("div"); //innehållet för bilder och input (siffror)
+    codeWrapper.append(codePanelIMGandNumbers);
+    codePanelIMGandNumbers.classList.add("codePanelIMGandNumbers");
+    
+    let codePanelWrapper = document.createElement("div"); 
+    codePanelWrapper.classList.add("codeNumbersWrapper");
+    codeWrapper.append(codePanelWrapper);
+
+    let codeinputnumbers = document.createElement("div");
+    codeinputnumbers.classList.add("codeinputnumbers");
+    codePanelIMGandNumbers.append(codeinputnumbers);
+    
+   
+    //skapar divar för kodpanelen 
+    for (let i= 1; i <= 9; i++) {
+        let codeNumbers = document.createElement("div");
+        codePanelWrapper.append(codeNumbers);
+        let numberString = i;
+        codeNumbers.classList.add("codeNumbers");
+        codeNumbers.innerHTML = numberString;
+
+        codeNumbers.addEventListener("click", () => { 
+            document.querySelector(".codeMessage").innerHTML ="";
+            let numberDiv = document.createElement("div"); 
+            let display = codeinputnumbers.innerHTML;
+            numberDiv.append(display); 
+            codeinputnumbers.innerHTML = `${display + numberString}`;
+        })
+    }    
+}
+
+codePanel();
+
+function codeControllerButton(){
+    let enterCodeButton = document.createElement("div");
+    document.querySelector(".codeWrapper").append(enterCodeButton);
+    enterCodeButton.classList.add("enterCodeButton");
+    enterCodeButton.innerHTML="ENTER CODE";
+    let password = `123456`;
+
+    enterCodeButton.addEventListener("click", () => {
+        let codeValues = document.querySelector(".codeinputnumbers").innerHTML;
+        console.log(codeValues);
+  
+        if(codeValues === password) {
+            console.log("this is right");
+        }else {
+            console.log("not right")
+            document.querySelector(".codeinputnumbers").innerHTML = "";
+            document.querySelector(".codeMessage").innerHTML ="This is not the right code!";
+
+        }
+    })
+}
+
+codeControllerButton();
+
+
+function clearPanel (clearButton){
+    clearButton = document.createElement("div");
+    clearButton.classList.add("clearCodePanelButton");
+    document.querySelector(".codeWrapper").append(clearButton);
+    clearButton.innerHTML="CLEAR CODE";
+    
+    clearButton.addEventListener("click", () => {
+        document.querySelector(".codeinputnumbers").innerHTML = "";
+    })
+
+}
+clearPanel();
+
+function createLaikasShip(){
+    let laikasShip = document.createElement("div");
+    laikasShip.classList.add("laikasShip");
+    document.getElementById("main").append(laikasShip);
+
+}
+createLaikasShip();
+
+
+
+document.querySelector(".laikasShip").addEventListener("click", function(){
+    console.log("click worked");
+    let element = document.querySelector(".codeWrapper");
+    element.classList.toggle("transformSize");
+});
+
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/styles.css">
+    <link rel="stylesheet" href="codepanel.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
@@ -13,10 +14,11 @@
     <body>
         <header>
         </header>
-        <main>
+        <main id="main">
         </main>
         <footer>
             <h4 id="footer-tile">© 2021 - Click n'Point studios</h4>
         </footer>
+        <script src="codepanel.js"></script>
     </body>
 </html>
