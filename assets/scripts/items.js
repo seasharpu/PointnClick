79b74c1230c594d4e10c8a53fc2929a45703b12e
AliceDuncalf <aliceduncalf@hotmail.se>
2021-12-29 15:01:05 +0100

    added temporary item pictures and eventlistener

--- assets/scripts/items.js
+++ assets/scripts/items.js
@@ -6,17 +6,26 @@ async function fetchItemsForPlanets () {
     const data = await response.json()
     let itemData = await data;
     //console.log(itemData);
+    let inventoryDiv = document.createElement("div");
+    document.querySelector(".background").prepend(inventoryDiv);
+    inventoryDiv.classList.add("inventoryDivtest");
+
     let itemsDiv = document.createElement("div");
     itemsDiv.innerHTML = "";
-    
+
     currentID.forEach(obj => {
         itemData.forEach(element => {
             if(element.id == obj) {
-               
                 itemsDiv.classList.add("planetsItem");
+                //itemsDiv.setAttribute("id", element.name);
                 document.querySelector(".background").prepend(itemsDiv);
-                itemsDiv.innerHTML = element.name;
+                //itemsDiv.innerHTML = element.name;
+                itemsDiv.style.backgroundImage = `url(${element.image})`;
             }
         });
+        itemsDiv.addEventListener("click", () => {
+            document.querySelector(".inventoryDivtest").append(itemsDiv);
+
+        })
     })
 }
