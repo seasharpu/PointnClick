283270bb66ea40f463ec314fdb8fe1304f28f4b7
olivialinnea <82369027+olivialinnea@users.noreply.github.com>
2021-12-30 15:07:36 +0100

    Uppdaterat css-styling och funktion

--- codepanel.js
+++ codepanel.js
@@ -1,10 +1,18 @@
 "use strict";
 
+function createLaikasShip(){
+    let laikasShip = document.createElement("div");
+    laikasShip.classList.add("laikasShip");
+    document.getElementById("main").append(laikasShip);
+
+}
+createLaikasShip();
+
 function codePanel() {
     let codeWrapper = document.createElement("div"); //main wrapper för innehållet
     document.getElementById("main").append(codeWrapper);
     codeWrapper.classList.add("codeWrapper");
-    codeWrapper.innerHTML =`<h1 id="codePanelTitel">UNLOCK LAIKAS SHIP
+    codeWrapper.innerHTML =`<h1 class="codePanelTitle">UNLOCK LAIKAS SHIP
                                 <p class= "codeMessage"></p>
                             </h1>`;
     
@@ -45,7 +53,7 @@ function codeControllerButton(){
     let enterCodeButton = document.createElement("div");
     document.querySelector(".codeWrapper").append(enterCodeButton);
     enterCodeButton.classList.add("enterCodeButton");
-    enterCodeButton.innerHTML="<p>ENTER CODE</p>";
+    enterCodeButton.innerHTML=`<p class="enterButton">ENTER CODE</p>`;
     let password = `123456`;
 
     enterCodeButton.addEventListener("click", () => {
@@ -70,7 +78,7 @@ function clearPanel (clearButton){
     clearButton = document.createElement("div");
     clearButton.classList.add("clearCodePanelButton");
     document.querySelector(".codeWrapper").append(clearButton);
-    clearButton.innerHTML="<p>CLEAR CODE</p>";
+    clearButton.innerHTML=`<p class="clearButton">CLEAR CODE</p>`;
     
     clearButton.addEventListener("click", () => {
         document.querySelector(".codeinputnumbers").innerHTML = "";
@@ -79,19 +87,47 @@ function clearPanel (clearButton){
 }
 clearPanel();
 
-function createLaikasShip(){
-    let laikasShip = document.createElement("div");
-    laikasShip.classList.add("laikasShip");
-    document.getElementById("main").append(laikasShip);
-
-}
-createLaikasShip();
-
-
 
 document.querySelector(".laikasShip").addEventListener("click", function(){
     console.log("click worked");
-    let element = document.querySelector(".codeWrapper");
-    element.classList.toggle("transformSize");
+    let wrapper = document.querySelector(".codeWrapper");
+    let numberWrapper = document.querySelector(".codeNumbersWrapper");
+    let panelTitle = document.querySelector(".codePanelTitle");
+    let enterCode = document.querySelector(".enterCodeButton");
+    let clearCode = document.querySelector(".clearCodePanelButton");
+
+    let enterButtonText = document.querySelector(".enterButton");
+    let clearButtonText = document.querySelector(".clearButton");
+    let codeNumber = document.getElementsByClassName("codeNumbers");
+
+    if (wrapper.classList.contains("transformSize")){
+        wrapper.classList.remove("transformSize");
+        numberWrapper.classList.remove("numberWrapperSize");
+        enterCode.classList.remove("enterCodeSize");
+        clearCode.classList.remove("clearCodeSize");
+        panelTitle.classList.remove("panelTitleSize");
+
+        enterButtonText.classList.remove("enterButtonSize");
+        clearButtonText.classList.remove("clearButtonSize");
+
+        for (let i = 0; i < codeNumber.length; i++){
+            codeNumber[i].classList.remove("numberSize");
+        }
+
+    } else {
+        wrapper.classList.add("transformSize");
+        numberWrapper.classList.add("numberWrapperSize");
+
+        enterCode.classList.add("enterCodeSize");
+        clearCode.classList.add("clearCodeSize");
+        panelTitle.classList.add("panelTitleSize");
+
+        enterButtonText.classList.add("enterButtonSize")
+        clearButtonText.classList.add("clearButtonSize");
+
+        for (let i = 0; i < codeNumber.length; i++){
+            codeNumber[i].classList.add("numberSize");
+        }
+    }
 });
 
