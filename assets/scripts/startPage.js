ebd5de2284bfc4b9c7f897bbdcf31bd71280d9e6
olivialinnea <82369027+olivialinnea@users.noreply.github.com>
2022-01-08 21:38:03 +0100

    uppdaterade styling på startsidan

--- assets/scripts/startPage.js
+++ assets/scripts/startPage.js
@@ -1,21 +1,30 @@
 "use strict";
 
 function makeStartPage() {
-    document.querySelector("main").style.backgroundImage = "url('assets/images/space_background.png')";
+    document.querySelector("main").style.backgroundImage = "url('assets/images/background2.gif')";
 
     let titleTextDiv = document.createElement("div");
+    let formContainerDiv = document.createElement("div");
     let titleText = document.createElement("h2");
+    let smallTitle = document.createElement("h1");
 
     titleTextDiv.classList.add("titleTextDiv");
+    formContainerDiv.classList.add("formContainerDiv");
     titleText.classList.add("titleText");
+    smallTitle.classList.add("smallTitleStart");
 
-    document.querySelector("main").append(titleTextDiv);
-    document.querySelector(".titleTextDiv").append(titleText);
+    document.querySelector("main").append(titleTextDiv, formContainerDiv);
+    document.querySelector(".titleTextDiv").append(titleText, smallTitle);
     titleText.innerHTML = "Saving Laika";
-
+    smallTitle.innerHTML = "return of the lost dog"
     let logInWrapper = document.createElement("div");
     logInWrapper.classList.add("logInWrapper");
-    document.querySelector("main").append(logInWrapper);
+
+    let planetMoon = document.createElement("div");
+    planetMoon.classList.add("planetMoon");
+    let laikaContain = document.createElement("div");
+    laikaContain.classList.add("laikaStart");
+    formContainerDiv.append(logInWrapper,planetMoon, laikaContain);
 
     let NameTagText = document.createElement("h2");
     NameTagText.classList.add("NameTagText");
