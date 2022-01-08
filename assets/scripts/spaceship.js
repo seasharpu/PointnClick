2bdcda2f97aeb35af1856542480cc49a7c5049df
murgrona <sawejonna@gmail.com>
2022-01-08 23:55:56 +0100

    sounds

--- assets/scripts/spaceship.js
+++ assets/scripts/spaceship.js
@@ -156,7 +156,7 @@ function backToSpaceship() {
     backToSpaceship.addEventListener("click", function() {
         // document.querySelector(".background").style.display = "none";
         loadingDivSpaceship();
-
+        clickSound();
         setTimeout(() => {
             //makePlanets();
             spaceShip();  
