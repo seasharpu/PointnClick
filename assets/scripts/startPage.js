20dfe57f7d535b82cd200a7059c1fe94c103b9e0
olivialinnea <82369027+olivialinnea@users.noreply.github.com>
2022-01-09 20:43:05 +0100

    raderat onödig kod

--- assets/scripts/startPage.js
+++ assets/scripts/startPage.js
@@ -26,35 +26,6 @@ function makeStartPage() {
     laikaContain.classList.add("laikaStart");
     formContainerDiv.append(logInWrapper,planetMoon, laikaContain);
 
-    // let NameTagText = document.createElement("h2");
-    // NameTagText.classList.add("NameTagText");
-    // document.querySelector(".logInWrapper").append(NameTagText);
-    // NameTagText.innerHTML = "NameTag";
-
-    // let NameTagInput = document.createElement("input");
-    // document.querySelector(".logInWrapper").append(NameTagInput);
-    // NameTagInput.setAttribute("ID", "nameTagInput");
-
-    // let PasswordText = document.createElement("h2");
-    // PasswordText.classList.add("PasswordText");
-    // document.querySelector(".logInWrapper").append(PasswordText);
-    // PasswordText.innerHTML = "Password";
-    // let PassWordInput = document.createElement("input");
-    // document.querySelector(".logInWrapper").append(PassWordInput);
-    // PassWordInput.setAttribute("ID", "passwordInput");
-
-    // let buttonsDiv = document.createElement("div");
-    // document.querySelector(".logInWrapper").append(buttonsDiv);
-    // buttonsDiv.classList.add("buttonsDiv");
-
-    // let newPlayerButton = document.createElement("div");
-    // let signUpButton = document.createElement("div");
-    // document.querySelector(".buttonsDiv").append(newPlayerButton, signUpButton);
-    // newPlayerButton.innerHTML = "<p>New player</p>";
-    // signUpButton.innerHTML = "<p>Sign in</p>";
-    // signUpButton.setAttribute("ID", "signInButton");
-    // newPlayerButton.setAttribute("ID", "newPlayerButton");
-
     let formDiv = document.createElement("div");
     formDiv.setAttribute("ID", "formWrapperDivStart");
     document.querySelector(".logInWrapper").append(formDiv);
