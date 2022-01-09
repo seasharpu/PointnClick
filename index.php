ad62003ca44222ca6082f74770ffb38eb5082102
MatNimv <matilda.nimvik@gmail.com>
2022-01-09 20:19:22 +0100

    mergad ihop med itemsV4.

--- index.php
+++ index.php
@@ -87,8 +87,8 @@ if (isset($_GET["id"])) {
             <script src="/assets/scripts/spaceship.js"></script>
             <script src="/assets/scripts/dialogue.js"></script>
             <script src="assets\scripts\statuscodemessages.js"></script>
-            <script src="/assets/scripts/pluto.js"></script>
             <script src="/assets/scripts/items.js"></script>
+            <script src="/assets/scripts/pluto.js"></script>
             <script src="/assets/scripts/loading.js"></script>
             <script src="assets\scripts\formPlanet.js"></script>
 
